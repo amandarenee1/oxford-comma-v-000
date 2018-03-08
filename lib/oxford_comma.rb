1a43ef3512a53d091321d17487eff459b97c4820
Amanda Wingate <arw0219@gmail.com>
2018-03-08 23:42:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,3 @@
 def oxford_comma(array)
-  if array.size < 2
-    return array.join
-  elsif array.size == 2
-    return array.insert(1, "and").join(" ")
-  else
-    str = ", and" + array.pop
-    str = array.join(", ") << str
-  end
+
 end
